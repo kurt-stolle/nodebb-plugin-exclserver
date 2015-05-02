c7a8085bdb9990957ec501b23e72ebd8f8a31649
Kurt Stolle <kurt@casualbananas.com>
2015-05-02 20:18:51 +0200

    Quick indentation fix

--- plugin.js
+++ plugin.js
@@ -1,8 +1,7 @@
 "use strict";
 
 // SCOPE VARIABLES
-var
-	plugin = {},
+var	plugin = {},
 	request = require('request');
 
 // SETTINGS
@@ -20,18 +19,16 @@ function renderAdmin(req, res, next) {
 function renderServers(req, res, next) {
 	request(Settings.get().api + '/servers/all', function(error,response,body) {
 	  if (!error && response.statusCode == 200) {
-	    res.render('exclserver/servers', {servers : JSON.parse(body)});
+	    res.render('exclserver/servers', { servers : JSON.parse(body) } );
 	  } else {
 			next(new Error("Invalid ExclServer API response."))
 		}
-	})
+	});
 }
 
 function renderDonate(req, res, next) {
 	res.render('exclserver/donate', {});
 }
-
-// SOCKET
 
 // HOOKS
 plugin.addAdminNavigation = function(header, callback) {
